bf892b7ff1e18b6427c0b0ca17316b3f136a9a6e
Gaby Karam <gkaramgk94@gmail.com>
2017-09-25 13:36:01 +0300

    ss

--- bot/keystone.js
+++ bot/keystone.js
@@ -23,7 +23,6 @@ keystone.init({
 	'custom engine': handlebars.create({
 		layoutsDir: 'templates/views/layouts',
 		partialsDir: 'templates/views/partials',
-		defaultLayout: 'default',
 		helpers: new require('./templates/views/helpers')(),
 		extname: '.hbs',
 	}).engine,
