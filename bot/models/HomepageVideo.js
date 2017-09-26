53bdcaed96b86a491a9faee2e719b0bcaa2948f0
koookiii <chriss.hadad@gmail.com>
2017-09-26 18:26:25 +0300

    Reediting video module

---
+++ bot/models/HomepageVideo.js
@@ -0,0 +1,18 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types
+
+/**
+ * Video Model
+ * ===========
+ */
+
+var HomepageVideo = new keystone.List('HomepageVideo',{
+    map: { name: 'link' },
+    label:'Homepage Video'
+});
+
+HomepageVideo.add({
+    link: { type: String, required: true },
+});
+
+HomepageVideo.register();
