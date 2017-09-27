c57eed630b1ea442f26e9d5924b94cfa9cefe991
koookiii <chriss.hadad@gmail.com>
2017-09-27 15:16:40 +0300

    Testimonial Model

---
+++ bot/models/Testimonial.js
@@ -0,0 +1,34 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Testimonial Model
+ * =================
+ */
+
+
+var Testimonial = new keystone.List('Partner',{
+    map:{name: 'name'},
+});
+
+var myStorage = new keystone.Storage({
+	adapter: keystone.Storage.Adapters.FS,
+	fs: {
+		path: keystone.expandPath('./public/images/testimonials'), // required; path where the files should be stored
+  		publicPath: '/images/testimonials/', // path where files will be served
+	},
+	schema: {
+		originalname: true,
+		url: true,
+		path: true,
+	},String
+});
+
+Testimonial.add({
+    name:{type:String, required:true},
+    jobPosition:{type:String},
+    testimonial:{type:String},
+    profileImage:{type: Types.File, storage: myStorage },
+});
+
+Testimonial.register();
