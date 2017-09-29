70ff6fc0b588e12009ccceba4ccd3a2896ca369e
koookiii <chriss.hadad@gmail.com>
2017-09-29 14:33:06 +0300

    added background image to how it works section

--- bot/models/HomepageHowItWorks.js
+++ bot/models/HomepageHowItWorks.js
@@ -10,9 +10,23 @@ var HomepageHowItWorks = new keystone.List('HomepageHowItWorks',{
     label:'How It Works'
 });
 
+var myStorage = new keystone.Storage({
+	adapter: keystone.Storage.Adapters.FS,
+	fs: {
+		path: keystone.expandPath('./public/images/howitworks'), // required; path where the files should be stored
+  		publicPath: '/images/howitworks/', // path where files will be served
+	},
+	schema: {
+		originalname: true,
+		url: true,
+		path: true,
+	},
+});
+
 HomepageHowItWorks.add({
     title: { type: String, default:'How It Works?' },
     text: { type: Types.Html, wysiwyg: true, height: 150 },
+    backgroundimage:{type: Types.File, storage: myStorage },    
 });
 
 HomepageHowItWorks.register();
