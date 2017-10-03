4369a23058de492527796c24c3600d7a39cf20f6
koookiii <chriss.hadad@gmail.com>
2017-10-03 13:13:49 +0300

    Changed post picture to local storage

--- bot/models/Post.js
+++ bot/models/Post.js
@@ -11,12 +11,25 @@ var Post = new keystone.List('Post', {
 	autokey: { path: 'slug', from: 'title', unique: true },
 });
 
+var myStorage = new keystone.Storage({
+	adapter: keystone.Storage.Adapters.FS,
+	fs: {
+		path: keystone.expandPath('./public/images/posts'), // required; path where the files should be stored
+  		publicPath: '/images/posts/', // path where files will be served
+	},
+	schema: {
+		originalname: true,
+		url: true,
+		path: true,
+	},
+});
+
 Post.add({
 	title: { type: String, required: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	author: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-	image: { type: Types.CloudinaryImage },
+	image: { type: Types.File, storage: myStorage},
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 },
