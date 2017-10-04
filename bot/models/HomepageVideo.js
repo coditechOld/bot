d72c1bfc074d6bda82552f087ca622eef751e062
koookiii <chriss.hadad@gmail.com>
2017-10-04 12:16:08 +0300

    Edited Video Model

--- bot/models/HomepageVideo.js
+++ bot/models/HomepageVideo.js
@@ -11,23 +11,9 @@ var HomepageVideo = new keystone.List('HomepageVideo',{
     label:'Homepage Video'
 });
 
-var myStorage = new keystone.Storage({
-	adapter: keystone.Storage.Adapters.FS,
-	fs: {
-		path: keystone.expandPath('./public/images/homepagevideobackground'), // required; path where the files should be stored
-  		publicPath: '/images/homepagevideobackground/', // path where files will be served
-	},
-	schema: {
-		originalname: true,
-		url: true,
-		path: true,
-	},
-});
-
 
 HomepageVideo.add({
     link: { type: String, required: true },
-    backgroundImage: { type: Types.File, storage: myStorage},
 });
 
 HomepageVideo.register();
