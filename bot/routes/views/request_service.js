55c367463b0f848dc934ba6406be0b1a0821c903
Gaby Karam <gkaramgk94@gmail.com>
2017-10-10 10:33:56 +0300

    Done training Page

--- bot/routes/views/request_service.js
+++ bot/routes/views/request_service.js
@@ -2,38 +2,38 @@ var keystone = require('keystone');
 var RequestService = keystone.list('RequestService');
 exports = module.exports = function (req, res) {
 
-    var view = new keystone.View(req, res);
-    var locals = res.locals;
-    // Set locals
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+	// Set locals
 
-    // locals.section is used to set the currently selected
-    // item in the header navigation.
-    locals.section = 'request-service';
-    locals.interestedTypes = RequestService.fields.interested.ops;
-    locals.contactBudgetTypes = RequestService.fields.budget.ops;
-    locals.formData = req.body || {};
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'request-service';
+	locals.interestedTypes = RequestService.fields.interested.ops;
+	locals.contactBudgetTypes = RequestService.fields.budget.ops;
+	locals.formData = req.body || {};
 
-    locals.data = {
-        posts: [],
-        header: {
-            background: null,
-            title: null,
-            text: null
-        }
-    };
-    view.on('init', function (next) {
+	locals.data = {
+		posts: [],
+		header: {
+			background: null,
+			title: null,
+			text: null,
+		},
+	};
+	view.on('init', function (next) {
 
-        var q = keystone.list('RequestServiceBanner').model.findOne();
+		var q = keystone.list('RequestServiceBanner').model.findOne();
 
-        q.exec(function (err, results) {
-            locals.data.header.background = results.image;
-            next(err);
-        });
-        console.log('Localsss 2 ==> ', locals.data.services);
+		q.exec(function (err, results) {
+			locals.data.header.background = results.image;
+			next(err);
+		});
+		console.log('Localsss 2 ==> ', locals.data.services);
 
 
-    });
+	});
 
-    // Render the view
-    view.render('request_services');
+	// Render the view
+	view.render('request_services');
 };
